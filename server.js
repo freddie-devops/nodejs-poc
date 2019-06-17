4d235e9a02305d62af18eed1fac32523f0684272
freddie-devops <51332673+freddie-devops@users.noreply.github.com>
2019-06-17 13:04:42 -0400

    test file

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world\n');
+  res.send('<center><p><font size="3" color="green">Welcome to FreddieMac Spinnaker POC \n by Pricing Devops Team<\p></font></center>\n <center>This is a simple Node Js Application</center> \n <center>we'll be using a very basic example: a Hello World server written with Node.</center>\n <center>This is a Successful Response...:)</center>\n');
 });
 
 app.listen(PORT, HOST);
